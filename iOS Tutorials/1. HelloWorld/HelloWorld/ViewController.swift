b1e2979db0ba6a717870a2d0a0b68eefceff18ba
Sharpkits Innovations <youtube@sharpkits.com>
2016-11-14 00:32:14 +0530

    iOS Tutorials: Introduction to Xcode 8

---
+++ iOS Tutorials/1. HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Sharpkits on 13/11/16.
+//  Copyright © 2016 Sharpkits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
